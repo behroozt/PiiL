a0cab54b411203e650da3feb8d221e9ab60eaed4
Behrooz Torabi <Behrooz@Behroozs-MacBook-Pro.local>
2016-06-02 23:31:26 +0200

    version 0.04 fixes and figures

--- src/piilSource/Interface.java
+++ src/piilSource/Interface.java
@@ -92,18 +92,18 @@ public class Interface extends JFrame{
 		
 		bodyFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);		
 		
-//		bodyFrame.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);		
-//		bodyFrame.addWindowListener(new java.awt.event.WindowAdapter() {
-//		    @Override
-//		    public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-//		        if (JOptionPane.showConfirmDialog(bodyFrame, 
-//		            "Are you sure to quit PiiL?", "Closing confirmation", 
-//		            JOptionPane.YES_NO_OPTION,
-//		            JOptionPane.QUESTION_MESSAGE, icon) == JOptionPane.YES_OPTION){
-//		            System.exit(0);
-//		        }
-//		    }
-//		});
+		bodyFrame.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);		
+		bodyFrame.addWindowListener(new java.awt.event.WindowAdapter() {
+		    @Override
+		    public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+		        if (JOptionPane.showConfirmDialog(bodyFrame, 
+		            "Are you sure to quit PiiL?", "Closing confirmation", 
+		            JOptionPane.YES_NO_OPTION,
+		            JOptionPane.QUESTION_MESSAGE, icon) == JOptionPane.YES_OPTION){
+		            System.exit(0);
+		        }
+		    }
+		});
 		
 		sampleInfoPanel = new JPanel();
 		sampleInfoPanel.setPreferredSize(new Dimension(1000, 30));
@@ -212,7 +212,7 @@ public class Interface extends JFrame{
 		gridConstraints.insets = new Insets(1,15,4,1);
 		
 		bodyFrame.add(backgroundPanel,BorderLayout.CENTER);
-//		new Splash(3000);	
+		new Splash(3000);	
 		bodyFrame.setVisible(true);
 		
 	}
