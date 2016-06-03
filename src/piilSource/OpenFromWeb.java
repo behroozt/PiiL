7100fc98c978d3cfaaedb1e9b7ce1ac04be80ea2
Behrooz Torabi <Behrooz@Behroozs-MacBook-Pro.local>
2016-06-03 08:14:38 +0200

    textfield click event added

--- src/piilSource/OpenFromWeb.java
+++ src/piilSource/OpenFromWeb.java
@@ -30,6 +30,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.beans.PropertyChangeListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -49,6 +50,7 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 import java.util.Map.Entry;
 
+import javax.swing.Action;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -173,7 +175,10 @@ public class OpenFromWeb extends JDialog{
 				organismCombo.setSelectedItem(organismDefault);
 			}
 			
-			public void keyPressed(KeyEvent arg0) {
+			public void keyPressed(KeyEvent ke) {
+				if (ke.getKeyCode() == KeyEvent.VK_ENTER){
+					loadButton.doClick();
+				}
 			}
 		});
 		
@@ -188,7 +193,10 @@ public class OpenFromWeb extends JDialog{
 				pathwayCombo.setSelectedItem(pathwayDefault);
 			}
 			
-			public void keyPressed(KeyEvent arg0) {
+			public void keyPressed(KeyEvent ke) {
+				if (ke.getKeyCode() == KeyEvent.VK_ENTER){
+					loadButton.doClick();
+				}
 			}
 		});
 		
